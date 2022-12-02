8ccbbb05a4b69314d9855b5b558f7dad29f66183
Rainer Neunteufel <rainer.neunteufel@gepardec.com>
2022-12-02 11:57:20 +0100

    aoc01 solution

---
+++ src/main/kotlin/aoc01/Solution01.kt
@@ -0,0 +1,35 @@
+package aoc01
+
+import java.nio.file.Paths
+
+fun main(args: Array<String>) {
+    val lines = Paths.get("src/main/resources/aoc_01.txt").toFile().useLines { it.toList() }
+    val linesAsInt = lines.map { it.toIntOrNull() };
+
+    println(solution1(linesAsInt))
+    println(solution2(linesAsInt))
+}
+
+fun solution1(calories: List<Int?>): Int? {
+    return solve(calories, 1);
+}
+
+fun solution2(calories: List<Int?>): Int? {
+    return solve(calories, 3);
+}
+
+fun solve(lines: List<Int?>, size: Int): Int? {
+    val caloriesByElf = mutableListOf<List<Int>>()
+    val caloriesElf = mutableListOf<Int>()
+
+    lines.forEach {
+        if (it == null) {
+            caloriesByElf.add(caloriesElf.toMutableList())
+            caloriesElf.clear()
+        } else {
+            caloriesElf.add(it)
+        }
+    }
+
+    return caloriesByElf.map { it.sum() }.sortedDescending().take(size).sum()
+}
